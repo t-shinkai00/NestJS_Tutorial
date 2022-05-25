815c06ddb3a7a4301f5e87441e56217364e6864a
t-shinkai00 <t.shinkai00@gmail.com>
2022-05-26 02:09:52 +0900

    add @Get, @Post, @Param

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Param } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get() // GET /cats で findAll が動作
+  findAll(): string {
+    return 'All cats';
+  }
+
+  @Post() // POST /cats で create が動作
+  create(): string {
+    return 'New cat';
+  }
+
+  @Get('mine') // GET /cats/mine で myCat が動作
+  myCat(): string {
+    return 'My cat';
+  }
+
+  @Get(':id') // GET /cats/:id で cat が動作
+  cat(@Param('id') id: string): string {
+    return `cat ${id}`;
+  }
+}
