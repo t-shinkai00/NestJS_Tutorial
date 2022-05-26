e8f372464eb5dac4030dedf0705cb36dd766ed8b
t-shinkai00 <t.shinkai00@gmail.com>
2022-05-26 14:01:28 +0900

    add @Header and @HttpCode

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -4,6 +4,8 @@ import { FastifyRequest } from 'fastify';
 @Controller('cats')
 export class CatsController {
   @Get() // GET /cats で findAll が動作
+  @Header('Foo', 'Bar') // Response Headerを設定できる
+  // @HttpCode(204) // HTTP Status Codeを変更できる
   findAll(@Req() request: FastifyRequest): string {
     console.log({ request });
     return 'All cats';
